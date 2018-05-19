98cf927275c0af7dc184933f6df89d760bd25d4c
Njose <Njose@DESKTOP-TKKG0TA>
2018-05-19 19:51:20 -0400

    driver

---
+++ JavaTron/src/javatron/javaTron.java
@@ -0,0 +1,41 @@
+package javatron;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class javaTron {
+
+	static JFrame frame;
+
+	static void menu() {
+		frame.dispose();
+
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setSize(size.width / 3, size.height);
+		frame.setLocation(size.width / 3, 0);
+		frame.setResizable(false);
+		JButton button = new JButton("START GAME");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				start();
+			}
+		});
+		frame.add(button);
+		frame.setVisible(true);
+	}
+
+	static void start() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
