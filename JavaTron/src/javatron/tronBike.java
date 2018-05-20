428ab8b197b04d82aef01770a81ab0ba3021d56d
elmo <elmo@172.20.10.4>
2018-05-20 13:28:13 -0400

    worked on tronBike class

--- JavaTron/src/javatron/tronBike.java
+++ JavaTron/src/javatron/tronBike.java
@@ -1,7 +1,23 @@
 package javatron;
 
-import java.awt.color.*;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class tronBike {
-
+	static final int RIGHT = 90;
+	static final int LEFT = 270;
+	static final int UP = 0;
+	static final int DOWN = 180;
+	
+	String name;
+	Color color;
+	int[] location;
+	boolean isAlive;
+	int direction;
+	
+	public static void tronBike(String name, Color color, int[] location) {
+		
+		
+	}
+	
 }
